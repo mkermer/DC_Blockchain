db83ecf06940b429c08da206c9d9092155879694
Morgane Kermer <kermer.morgane@gmail.com>
2021-02-24 17:35:08 +0100

    Favicon and Navbar Logo

--- DC_FrontEnd/src/Components/Navbar/NavbarDC.js
+++ DC_FrontEnd/src/Components/Navbar/NavbarDC.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Logo from '../../Logo/DCoin1.svg';
+import LogoText from '../../Logo/DCoinText.svg';
+import './NavbarDC.css';
 
 class NavbarDC extends Component {
     render() {
         return (
             <Navbar bg="dark" variant="dark" sticky="top" collapseOnSelect expand="lg">
-                <Link to="/" className="navbar-brand">DC Blockchain</Link>
-
-                <Nav className="mr-auto">
-                    <Link to="/" className="navbar-brand">Home</Link>
-                </Nav>
+                <Link to="/" className="navbar-brand">
+                    <img
+                    src={Logo}
+                    width="50"
+                    height="50"
+                    className="d-inline-block align-top"
+                    alt="DCoin Logo"
+                    />  
+                </Link>
+                <Link to="/" className="navbar-brand">
+                <img
+                    src={LogoText}
+                    height="50"
+                    className="d-inline-block align-top"
+                    alt="DCoin Text Logo"
+                    />
+                </Link>
+                
 
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                
+
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Form inline>
                         <Link to="/registerKeys">
