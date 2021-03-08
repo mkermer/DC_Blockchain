f71d6d30b77e0c668db854c6b4ee5dcb4a3e602d
Morgane Kermer <kermer.morgane@gmail.com>
2021-03-08 13:13:46 +0100

    frontend

--- DC_BackEnd/routes/login.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const handleSignIn = require("../controllers/authController");
-
-
-
-router.post("/login", handleSignIn);
-
-module.exports = router;
---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+export default {
+    baseUrl: 'https://dc-blockchain-server.herokuapp.com'
+}
