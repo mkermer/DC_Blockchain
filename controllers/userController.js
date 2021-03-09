04ea85e2f480fc6bd0a1aaf44df482eb06b7aaac
Morgane Kermer <kermer.morgane@gmail.com>
2021-03-09 21:13:46 +0100

    added logging

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,10 +13,10 @@ const createUser = async (req, res) => {
 
     try {
         const jwt = user.generateJWT();
-        console.warn('user jwt generated!');
+        console.error('user jwt generated!');
         await user.save();
 
-        console.warn('user saved!');
+        console.error('user saved!');
 
         return res.status(201).json({
             success: true,
